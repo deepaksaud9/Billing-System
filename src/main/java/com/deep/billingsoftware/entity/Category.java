9aabd68e957ad94cf150bdbbe6360eb66d29d233
Deepak Saud <deppofficial00@gmail.com>
2025-07-06 19:04:00 +0545

    commit helper class

--- src/main/java/com/deep/billingsoftware/entity/Category.java
+++ src/main/java/com/deep/billingsoftware/entity/Category.java
@@ -4,10 +4,12 @@ import com.deep.billingsoftware.dtos.auditing.Auditable;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 @Builder
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
--- src/main/java/com/deep/billingsoftware/service/serviceImpl/CategoryServiceImpl.java
+++ src/main/java/com/deep/billingsoftware/service/serviceImpl/CategoryServiceImpl.java
@@ -3,22 +3,47 @@ package com.deep.billingsoftware.service.serviceImpl;
 import com.deep.billingsoftware.dtos.request.CategoryRequest;
 import com.deep.billingsoftware.dtos.response.CategoryResponse;
 import com.deep.billingsoftware.entity.Category;
+import com.deep.billingsoftware.repository.CategoryRepository;
 import com.deep.billingsoftware.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
     @Override
     public CategoryResponse saveCategory(CategoryRequest request) {
 
-        Category category = Category.builder()
+        Category category = convertToCategory(request);
+        Category response = categoryRepository.save(category);
+
+        return null;
+
+    }
+
+
+    private Category convertToCategory(CategoryRequest request){
+        return Category.builder()
+                .categoryId(UUID.randomUUID().toString())
                 .categoryName(request.getCategoryName())
                 .bgColor(request.getBgColor())
                 .description(request.getDescription())
                 .build();
+    }
 
-        return null;
+    private CategoryResponse convertToCategoryResponse(Category category){
+        return CategoryResponse.builder()
+                .id(category.getId())
+                .categoryId(category.getCategoryId())
+                .categoryName(category.getCategoryName())
+                .imgUrl(category.getImgUrl())
+                .bgColor(category.getBgColor())
+                .description(category.getDescription())
+                .build();
     }
 }
